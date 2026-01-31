b61619e34e911198c18835d8d020b2b933988748
Lucy Thien Hang Ziener <lucyziener@gmail.com>
2026-01-31 18:05:35 +0100

    FIXED: a bug with the decor not spawning

--- Assets/Lucy_Test/DecorLogic.cs
+++ Assets/Lucy_Test/DecorLogic.cs
@@ -10,7 +10,7 @@ public class DecorLogic : MonoBehaviour
         spawnedobj = new GameObject("Detector");
         //Add Components
         spawnedobj.AddComponent<BoxCollider>();
-        spawnedobj.GetComponent<BoxCollider>().isTrigger = true;
+        //spawnedobj.GetComponent<BoxCollider>().isTrigger = true;
         spawnedobj.tag = "Decor";
         spawnedobj.transform.position = gameObject.transform.position;
         spawnedobj.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
