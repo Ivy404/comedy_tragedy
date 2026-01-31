e72b43c164f7f074e72cbb64a47767f81ad101eb
Lucy Thien Hang Ziener <lucyziener@gmail.com>
2026-01-31 11:14:57 +0100

    ADDED: floor spawning on game start

--- Assets/Lucy_Test/FloorSpawn_Manager.cs
+++ Assets/Lucy_Test/FloorSpawn_Manager.cs
@@ -7,6 +7,7 @@ public class FloorSpawn_Manager : MonoBehaviour
 
     public GameObject player;
     private Vector3 playerPos;
+
     private int tileCount;
 
     void Start()
@@ -14,7 +15,19 @@ public class FloorSpawn_Manager : MonoBehaviour
         tileCount = TileTypes.Length;
         playerPos = player.transform.position;
 
-        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], playerPos); // Spawn the 9 tiles
+        // Spawn the 9 tiles
+        //mid
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3 (playerPos.x, 0, playerPos.z + 10), new Quaternion (0,0,0,0));
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3(playerPos.x, 0, playerPos.z), new Quaternion(0, 0, 0, 0));
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3(playerPos.x, 0, playerPos.z - 10), new Quaternion(0, 0, 0, 0));
+        //right
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3 (playerPos.x + 10, 0, playerPos.z + 10), new Quaternion(0, 0, 0, 0));
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3(playerPos.x + 10, 0, playerPos.z), new Quaternion(0, 0, 0, 0));
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3(playerPos.x + 10, 0, playerPos.z - 10), new Quaternion(0, 0, 0, 0));
+        //left
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3(playerPos.x - 10, 0, playerPos.z + 10), new Quaternion(0, 0, 0, 0));
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3(playerPos.x - 10, 0, playerPos.z), new Quaternion(0, 0, 0, 0));
+        Instantiate(TileTypes[Random.Range(0, TileTypes.Length)], new Vector3(playerPos.x - 10, 0, playerPos.z - 10), new Quaternion(0, 0, 0, 0));
     }
 
     void Update()
